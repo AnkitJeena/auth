92b0b799de154c5d17edf9ad80cc9c1e60d3498c
Ankit Jeena <ankitjeena.08@gmail.com>
2024-08-31 00:14:23 +0530

    changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-auth-cbb6d.firebaseapp.com",
-  projectId: "mern-auth-cbb6d",
-  storageBucket: "mern-auth-cbb6d.appspot.com",
-  messagingSenderId: "848381684330",
-  appId: "1:848381684330:web:14d3d4292ac8f05a99f813"
+  authDomain: "mern-auth-54338.firebaseapp.com",
+  projectId: "mern-auth-54338",
+  storageBucket: "mern-auth-54338.appspot.com",
+  messagingSenderId: "1051827590071",
+  appId: "1:1051827590071:web:e8c25c3dc3a26e85618e32"
 };
 
 export const app = initializeApp(firebaseConfig);
